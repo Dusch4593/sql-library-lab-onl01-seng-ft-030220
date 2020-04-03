6e722688902984c0ee522a28d192bd5ad644b994
Brandon Dusch <brandondusch@gmail.com>
2020-04-03 03:11:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,10 +28,10 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  FROM series
-  JOIN characters 
-  ON characters.author_id = series.author_id AND characters.species = 'human'
-  LIMIT 1"
+   FROM series
+   JOIN characters 
+   ON characters.author_id = series.author_id AND characters .species = 'human'
+   LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
